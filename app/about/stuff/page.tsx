36cc09363493feb1086f72b902c1782d7dd3ae87
2025program <suiranprogram@gmail.com>
2025-03-26 00:04:09 +0900

    className="member-name"の設定（どこで指定すればいいかわかりません）

--- app/about/stuff/page.tsx
+++ app/about/stuff/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 import { useEffect, useCallback } from "react";
 import { useBreadcrumb } from "@/components/bread/BreadcrumbContext";
+import { color } from "framer-motion";
+import { Color } from "three/src/Three.js";
 
 export default function Stuff() {
     const { setBreadcrumbs } = useBreadcrumb();
@@ -22,7 +24,7 @@ export default function Stuff() {
 
             <section　className="mb-6">
                 <h2>翠翔祭について</h2>
-                <a>翠翔祭とは毎年6月末に行われる神奈川県立横浜翠嵐高校の文化祭です。コロナの年度は一般公開できなかったものの、去年から一般公開を再開し、約１万人の方々に来場いただきました。ぜひともご来場お願いします。</a>
+                <a>翠翔祭とは毎年6月末に行われる神奈川県立横浜翠嵐高校の文化祭です。コロナの年度は一般公開できなかったものの、去年から一般公開を再開し、約１万人の方々に来場いただきました。来場お待ちしております。</a>
             </section>
 
             <section className="mb-6">
@@ -38,28 +40,28 @@ export default function Stuff() {
             <section className="mb-6">
                 <h2 className="text-xl mb-2">翠翔祭実行委員会総務部紹介</h2>
                 <h3>| 実行委員長</h3>
-                <a>　池田晴彦</a>
+                <a className="member-name">　池田晴彦</a>
                 <h3>| 副委員長兼会計長</h3>
-                <a>　山口理緒</a>
+                <a className="member-name">　山口理緒</a>
                 <h3>| 会場長</h3>
-                <a>　深堀絢心</a>
+                <a className="member-name">　深堀絢心</a>
                 <h3>| 調理食販部統括責任者</h3>
-                <a>　東城英寿</a>
+                <a className="member-name">　東城英寿</a>
                 <h3>| プログラム部統括責任者</h3>
-                <a>　水谷駿佑</a>
+                <a className="member-name">　水谷駿佑</a>
                 <h3>| アーチ・広報部統括責任者</h3>
-                <a>　浅井心遙</a>
+                <a className="member-name">　浅井心遙</a>
                 <h3>| SP部統括責任者</h3>
-                <a>　𠮷岡太郎</a>
+                <a className="member-name">　𠮷岡太郎</a>
                 <h3>| 後夜祭・ステージ部統括責任者</h3>
-                <a>　富永恵未</a>
+                <a className="member-name">　富永恵未</a>
             </section>
 
             <section className="mb-6">
                 <h2 className="text-xl mb-2">ウェブサイト開発</h2>
-                <p>
+                <a className="member-name">
                     Pent &#40;横浜翠嵐所属&#41;
-                </p>
+                </a>
             </section>
 
             
